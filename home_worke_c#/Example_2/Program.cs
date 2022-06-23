fe3a29c95b8914d625d5b4acaec762bddfaee56d
valery_ryzhkov <valery.ryzhkov@gmail.com>
2022-06-23 22:54:05 +0300

    add home work#1

---
+++ home_worke_c#/Example_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход три числа и 
+//выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+int a = 22;
+int b = 3;
+int c = 9;
+int biggre = 0;
+if (a > biggre) biggre = a;
+if (b > biggre) biggre = b;
+if (c > biggre) biggre = c;
+Console.Write("это число ");
+Console.Write(biggre);
---
+++ home_worke_c#/Example_4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+Console.WriteLine("введите целое число");
+int numN = int.Parse(Console.ReadLine());
+int numN =>1;
+int numN <= numN;
+if (numN >0)
+{
+    while (count <= numN)
+    {
+        Console.WriteLine(count +1);
+        count = count+1;
+    }
+}
